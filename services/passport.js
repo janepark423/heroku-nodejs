3bbc69a09ded1739d3cb0a8c3d9fba7117d5b105
janeapark423 <janepark423@gmail.com>
2022-10-06 17:29:57 +0900

    몽고쿼리

--- services/passport.js
+++ services/passport.js
@@ -13,17 +13,15 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }) //몽고db에서 쿼리 불러오기
-                .then((existingUser) => {
-                    if (existingUser) {
-                        //existingUser가 존재한다면,
-                    } else {
-                        //해당 id가 존재하지 않으니 새로 만들어라.
-                    }
+            User.findOne({ googleId: profile.id }).then((existingUser) => {
+                if (existingUser) {
+                    //existingUser가 이미 존재한다면 만들지 말아라.,
+                    done(null, existingUser);
+                } else {
+                    //해당 id가 존재하지 않으니 새로 만들어라.
+                    new User({ googleId: profile.id }).save();
                 }
-                })
-
-            new User({ googleId: profile.id }).save();
+            });
         }
     )
 );
