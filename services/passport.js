4eddad325f5d925abbda0c994050a424695f77bc
janeapark423 <janepark423@gmail.com>
2022-10-06 17:50:08 +0900

    cookie session 적용

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,19 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user);
+        });
+});
+
+
+
 passport.use(
     new GoogleStrategy(
         {
@@ -13,13 +26,15 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            User.findOne({ googleId: profile.id }).then((existingUser) => {
+            User.findOne({ googleId: profile.id }).then(existingUser => {
                 if (existingUser) {
                     //existingUser가 이미 존재한다면 만들지 말아라.,
                     done(null, existingUser);
                 } else {
                     //해당 id가 존재하지 않으니 새로 만들어라.
-                    new User({ googleId: profile.id }).save();
+                    new User({ googleId: profile.id })
+                        .save()
+                        .then(user => done(null, user));
                 }
             });
         }
